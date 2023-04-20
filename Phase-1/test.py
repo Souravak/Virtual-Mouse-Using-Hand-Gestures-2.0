05b6feaf8baf8d889fa961f86c35ec35d74786fe
souravak <souravak211@gmail.com>
2023-04-20 14:11:08 +0530

    added file v5

--- Phase-1/test.py
+++ Phase-1/test.py
@@ -1,4 +1,35 @@
-from datetime import datetime
-now = datetime.now()
+# import pyautogui as pg
+# import time
+# for i in range(5):
+#     # press function key
+#     pg.press('brightnessup')
 
-print(type(now.second))
+# import wmi
+# import time
+# c = wmi.WMI(namespace='wmi')
+# methods = c.WmiMonitorBrightnessMethods()[0]   
+# for i in range(0,100):
+#     brightness = i # percentage [0-100] For changing thee screen 
+#     methods.WmiSetBrightness(brightness, 0)
+#     time.sleep(0.5)
+
+
+x = 10
+
+
+def changeX():
+    global x
+    print("in function", x)
+
+
+def changeXAgain():
+    global x
+    x += 100
+
+def changeXAgain():
+
+print(x)
+changeX()
+print(x)
+changeXAgain()
+print(x)
